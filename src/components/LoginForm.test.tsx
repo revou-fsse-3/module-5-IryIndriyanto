1d0a558a99b014541b9888ebce90beb900bdad3a
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-01-25 05:34:14 -0700

    fix : test case for error validation

--- src/components/LoginForm.test.tsx
+++ src/components/LoginForm.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen, fireEvent, act, } from '@testing-library/react';
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import userEvent from '@testing-library/user-event';
 import LoginForm from './LoginForm'
 import { useRouter } from 'next/router'
 
@@ -21,18 +20,19 @@ test('renders login form', () => {
   expect(loginButton).toBeInTheDocument()
 })
 
- test('error validation in login form', async () => {
-  render(<LoginForm />);
-  const emailInput = screen.getByPlaceholderText('Input Your Email');
-  const passwordInput = screen.getByPlaceholderText('Input Your Password');
-  const loginButton = screen.getByRole('button');
+test('error validation in login form', async () => {
+  render(<LoginForm />)
+  const emailInput = screen.getByPlaceholderText('Input Your Email')
+  const passwordInput = screen.getByPlaceholderText('Input Your Password')
+  const loginButton = screen.getByRole('button')
 
   // Simulate submitting the form without entering email and password
-  act(() => {
-    userEvent.click(loginButton);
-  });
 
+  fireEvent.click(loginButton)
+
+  await waitFor(() => {
+    expect(screen.getByText('email is a required field')).toBeInTheDocument()
+    expect(screen.getByText('password is a required field')).toBeInTheDocument()
+  })
   // Assertion to check if error messages are displayed
-  expect(screen.getByText('email is a required field')).toBeInTheDocument();
-  expect(screen.getByText('password is a required field')).toBeInTheDocument();
-});
+})
